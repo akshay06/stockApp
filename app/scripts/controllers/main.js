6b347fa630c87e4dd58504be7669ab5d1ad458b7
Akshay Jain <akshayjain0603@yahoo.com>
2017-11-08 02:28:06 +0530

    no results found

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,12 @@
 'use strict';
 angular.module('stockApp')
 
-.controller('MainController', function ($scope, StockData) {
+.controller('MainController', function ($scope, StockData, $timeout) {
   $scope.StockData = StockData;
   $scope.searchInput = '';
+  $scope.searchStock = function() {
+    $timeout(function () {
+      $scope.showNoResult = angular.element('.keyItem').length === 0;
+    }, 0);
+  };
 });
