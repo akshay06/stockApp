3cdf4576b887fef9ebfc56af12b6dd1b4792e9a8
Akshay Jain <akshayjain0603@yahoo.com>
2017-11-08 01:41:36 +0530

    Search added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,48 +1,7 @@
 'use strict';
 angular.module('stockApp')
-.factory('MyData', function($websocket) {
-  // Open a WebSocket connection
-  var dataStream = $websocket('ws://stocks.mnet.website');
 
-  var collection = [];
-  var requiredObj = {};
-  function dateFn() {
-    var d = new Date();
-    var h = ('0' + d.getHours()).slice(-2);
-    var m = ('0' + d.getMinutes()).slice(-2);
-    var s = ('0' + d.getSeconds()).slice(-2);
-    var ms = ('0' + d.getMilliseconds()).slice(-2);
-    return h + ':' + m + ':' + s + ':' + ms;
-}
-  dataStream.onMessage(function(message) {
-    collection.push(JSON.parse(message.data));
-    angular.forEach(collection, function(valueArray, key) {
-      angular.forEach(valueArray, function(value, key) {
-        if(!requiredObj[value[0]]) {
-          requiredObj[value[0]] = {};
-          requiredObj[value[0]].oldValue = value[1];
-          requiredObj[value[0]].newValue = value[1];
-          requiredObj[value[0]].newTime = dateFn();
-        } else {
-          requiredObj[value[0]].oldValue = angular.copy(requiredObj[value[0]].newValue);
-          requiredObj[value[0]].newValue = value[1];
-          if(requiredObj[value[0]].oldValue !== requiredObj[value[0]].newValue) {
-            requiredObj[value[0]].newTime = dateFn();
-          }
-        }
-      });
-    });
-  });
-
-  var methods = {
-    requiredObj: requiredObj,
-    get: function() {
-      dataStream.send(JSON.stringify({ action: 'get' }));
-    }
-  };
-  return methods;
-})
-.controller('MainController', function ($scope, MyData) {
-  $scope.MyData = MyData;
-
+.controller('MainController', function ($scope, StockData) {
+  $scope.StockData = StockData;
+  $scope.searchInput = '';
 });
---
+++ app/scripts/services/stockData.js
@@ -0,0 +1,46 @@
+'use strict';
+angular.module('stockApp')
+.factory('StockData', function($websocket) {
+  // Open a WebSocket connection
+  var dataStream = $websocket('ws://stocks.mnet.website');
+
+  var collection = [];
+  var requiredObj = {};
+  function dateFn() {
+    var d = new Date();
+    var h = ('0' + d.getHours()).slice(-2);
+    var m = ('0' + d.getMinutes()).slice(-2);
+    var s = ('0' + d.getSeconds()).slice(-2);
+    var ms = ('0' + d.getMilliseconds()).slice(-2);
+    return h + ':' + m + ':' + s + ':' + ms;
+}
+  dataStream.onMessage(function(message) {
+    collection.push(JSON.parse(message.data));
+    angular.forEach(collection, function(valueArray, key) {
+      angular.forEach(valueArray, function(value, key) {
+        if(!requiredObj[value[0]]) {
+          requiredObj[value[0]] = {};
+          requiredObj[value[0]].oldValue = value[1];
+          requiredObj[value[0]].newValue = value[1];
+          requiredObj[value[0]].newTime = Date.now();
+          requiredObj[value[0]].prevTime = angular.copy(requiredObj[value[0]].newTime);
+        } else {
+          requiredObj[value[0]].oldValue = angular.copy(requiredObj[value[0]].newValue);
+          requiredObj[value[0]].newValue = value[1];
+          if(requiredObj[value[0]].oldValue !== requiredObj[value[0]].newValue) {
+            requiredObj[value[0]].prevTime = angular.copy(requiredObj[value[0]].newTime);
+            requiredObj[value[0]].newTime = Date.now();
+          }
+        }
+      });
+    });
+  });
+
+  var methods = {
+    requiredObj: requiredObj,
+    get: function() {
+      dataStream.send(JSON.stringify({ action: 'get' }));
+    }
+  };
+  return methods;
+});
